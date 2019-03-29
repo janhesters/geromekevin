ae286e5a5cc1fa4143bf2f922023a989cddb9333
Jan Hesters <jan.hesters@rwth-aachen.de>
2019-03-29 10:47:07 +0100

    Add light and dark theme.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
+import Helmet from "react-helmet"
 
 import { rhythm, scale } from "../utils/typography"
 import { primaryColor } from "../style"
+import Toggle from "./Toggle"
+import sun from "../../content/assets/sun.png"
+import moon from "../../content/assets/moon.png"
 
 class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
+  state = {
+    theme: null,
+  }
+  componentDidMount() {
+    this.setState({ theme: window.__theme })
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme })
+    }
+  }
+  renderHeader() {
+    const { location, title } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
     if (location.pathname === rootPath) {
-      header = (
+      return (
         <h1
           style={{
             ...scale(0.75),
@@ -21,18 +33,18 @@ class Layout extends React.Component {
         >
           <Link
             style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
+              boxShadow: "none",
+              textDecoration: "none",
+              color: "var(--textTitle)",
             }}
-            to={`/`}
+            to={"/"}
           >
             {title}
           </Link>
         </h1>
       )
     } else {
-      header = (
+      return (
         <h3
           style={{
             fontFamily: "Montserrat, sans-serif",
@@ -55,26 +67,80 @@ class Layout extends React.Component {
         </h3>
       )
     }
+  }
+  render() {
+    const { children } = this.props
+
     return (
       <div
         style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          color: "var(--textNormal)",
+          background: "var(--bg)",
+          transition: "color 0.2s ease-out, background 0.2s ease-out",
+          minHeight: "100vh",
         }}
       >
-        <header
+        <Helmet
+          meta={[
+            {
+              name: "theme-color",
+              content:
+                this.state.theme === "light" ? "#82AAFF" : "#282c35",
+            },
+          ]}
+        />
+        <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: "2.625rem",
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: rhythm(24),
+            padding: `2.625rem ${rhythm(3 / 4)}`,
           }}
         >
-          {header}
-        </header>
-        {children}
+          <header
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: "2.625rem",
+            }}
+          >
+            {this.renderHeader()}
+            {this.state.theme !== null ? (
+              <Toggle
+                icons={{
+                  checked: (
+                    <img
+                      src={moon}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: "none" }}
+                    />
+                  ),
+                  unchecked: (
+                    <img
+                      src={sun}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: "none" }}
+                    />
+                  ),
+                }}
+                checked={this.state.theme === "dark"}
+                onChange={e =>
+                  window.__setPreferredTheme(
+                    e.target.checked ? "dark" : "light"
+                  )
+                }
+              />
+            ) : (
+              <div style={{ height: "24px" }} />
+            )}
+          </header>
+          {children}
+        </div>
       </div>
     )
   }
